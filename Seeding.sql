7c826458984923bfae9319eecfe8210f89864ab8
IrynaSoltyk <irishkam@hotmail.com>
2021-12-20 11:05:45 +0200

    Data seeding bug fixed

--- Seeding.sql
+++ Seeding.sql
@@ -4,9 +4,9 @@ insert into users (enabled, login, password, username)
 values (true, 'cashier', '$2a$12$K701OtpwkwkMQdUqCbXRRen0YqNii/uSP9LxtIhBjqiY8FQdS46NK', 'Людмила Іванівна Горобець');
 insert into users (enabled, login, password, username)
 values (true, 'cashiermanager', '$2a$12$K701OtpwkwkMQdUqCbXRRen0YqNii/uSP9LxtIhBjqiY8FQdS46NK', 'Василіса Йосипівна Жук');
-insert into roles (name) values ('COMMODITYEXPERT');
-insert into roles (name) values ('CASHIER');
-insert into roles (name) values ('CASHIERMANAGER');
+insert into roles (name) values ('ROLE_COMMODITYEXPERT');
+insert into roles (name) values ('ROLE_CASHIER');
+insert into roles (name) values ('ROLE_CASHIERMANAGER');
 insert into users_roles (user_id, role_id) values (1,1);
 insert into users_roles (user_id, role_id) values (2,2);
 insert into users_roles (user_id, role_id) values (3,3);
