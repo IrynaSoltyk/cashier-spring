2d8438591641f30d9c7e127c0ebd55b443b4fe69
IrynaSoltyk <irishkam@hotmail.com>
2021-12-20 11:39:40 +0200

    units values seeding added

--- Seeding.sql
+++ Seeding.sql
@@ -10,3 +10,6 @@ insert into roles (name) values ('ROLE_CASHIERMANAGER');
 insert into users_roles (user_id, role_id) values (1,1);
 insert into users_roles (user_id, role_id) values (2,2);
 insert into users_roles (user_id, role_id) values (3,3);
+insert into units (name) values ('kg');
+insert into units (name) values ('litre');
+insert into units (name) values ('piece');
